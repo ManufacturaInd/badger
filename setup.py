961f5d49f48c2f780a2968c677452c209f60c7c2
Ricardo Lafuente <r@manufacturaindependente.org>
2014-05-03 01:12:35 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='badger',
+      version='0.1',
+      description='Text to bitmap image converter, designed to render ASCII art using bitmap fonts',
+      author='Ricardo Lafuente',
+      author_email='r@manufacturaindependente.org',
+      url='http://github.com/manufacturaind/badger/',
+      download_url='https://github.com/ManufacturaInd/badger/tarball/master',
+      scripts=['badger'],
+      license="GPL",
+     )
+
